4fe0fb8d401629c9da3d753d0da19b18c606f095
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-09 21:25:51 +0100

    after method for badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,20 +11,28 @@ class Counter extends Component {
     }
 
 
-
     render() {
 
         return (
 
             <div className=' mt-2'>
                 <h3>Shopping Basket</h3>
-                <span style={this.styles} className='badge badge-primary ml-5 mr-2'> {this.formatcount()} </span>
+                <span style={this.styles} className={this.getBadgeClasses()}> {this.formatcount()} </span>
                 <button className='btn btn-secondary btn-sm' >Buy more</button>
             </div>
         );
 
     }
+
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
     //Methods Place
+
     formatcount() {
         const count = this.state.count
         return count === 0 ? 'Zero' : count;
