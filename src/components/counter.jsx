860ba8ef3b4168cf27dc1b50cfec4f3b6bcf54af
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-09 19:18:47 +0100

    5

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-
-
-class Counter extends Component {
-
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Shopping Basket</h1>
-                <button className='btn-primary' >Buy more</button>
-            </React.Fragment>
-        );
-
-    }
-}
-
-export default Counter;
-=======
-<<<<<<< HEAD
 import React, { Component } from 'react';
 
 
@@ -35,23 +15,4 @@ class Counter extends Component {
 }
 
 export default Counter;
-=======
-import React, { Component } from 'react';
 
-
-class Counter extends Component {
-
-    render() {
-        return (
-            <div>
-                <h1>Shopping Basket</h1>
-                <button className='btn-primary' >Buy more</button>
-            </div>
-        );
-
-    }
-}
-
-export default Counter;
->>>>>>> 5ef6c751d924f1b10c30114dfbbc3482f1e20311
->>>>>>> e34da29716046d3b3a821885fbf9c19eefa5354a
