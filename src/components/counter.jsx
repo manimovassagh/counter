fc2af0b1425efdd647845002fbb2c765951f79dc
Mani Movassagh <74688476+manimovassagh@users.noreply.github.com>
2021-02-09 19:11:44 +0100

    Add files via upload

---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Counter extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Shopping Basket</h1>
+                <button className='btn-primary' >Buy more</button>
+            </div>
+        );
+
+    }
+}
+
+export default Counter;
