71c20d340c08a85155be7dc0aa73f2a4719110a7
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-09 21:16:04 +0100

    after styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,26 +4,29 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        address: {
-            strasse: 'lindengassse',
-            housenummer: '7'
-
-        }
+    };
+    styles = {
+        fontSize: '15px',
+        fontWeight: 'bold'
     }
+
+
+
     render() {
 
         return (
-            <div>
-                <span>{this.state.count}</span>
-                <button className='btn-primary' >Buy more</button>
+
+            <div className=' mt-2'>
+                <h3>Shopping Basket</h3>
+                <span style={this.styles} className='badge badge-primary ml-5 mr-2'> {this.formatcount()} </span>
+                <button className='btn btn-secondary btn-sm' >Buy more</button>
             </div>
         );
 
     }
-
-    //methods place
+    //Methods Place
     formatcount() {
-        const count=this.state.count
+        const count = this.state.count
         return count === 0 ? 'Zero' : count;
     }
 }
