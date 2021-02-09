a0fa9b4bb9ae87dacb9ecd79ab583f6ba52b6259
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-09 19:13:32 +0100

    after adding button

---
+++ src/components/counter.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Counter extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Shopping Basket</h1>
+                <button className='btn-primary' >Buy more</button>
+            </React.Fragment>
+        );
+
+    }
+}
+
+export default Counter;
