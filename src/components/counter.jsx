c1e18fe7f21aead576b4f55e86367a80d9b93c46
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-09 19:21:49 +0100

    state added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 
 class Counter extends Component {
+    state = {
+        count: 0,
+        address: {
+            strasse: 'lindengassse',
+            housenummer: '7'
 
+        }
+    }
     render() {
+
         return (
             <div>
                 <h1>Shopping Basket</h1>
